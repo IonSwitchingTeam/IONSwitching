0f214d02d3afe4c61e7d8b136aea39bc02566d2d
eokon <okonedidiong@outlook.com>
2020-03-31 20:27:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # pip install --upgrade tensorflow
 # pip install keras
 
-from tensorflow.keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping
 from tensorflow.keras.datasets import imdb
-from tensorflow.keras.preprocessing import sequence
+from keras.preprocessing import sequence
 
 from keras_model import RCNNVariant
 
 def main():
-    max_features = 5000 #maximum number of words in training set
-    maxlen = 400 #maximum number of words in each document
+    max_features = 90000 #maximum number of words in training set
+    max_len = 200 #maximum number of words in each document
     batch_size = 32
-    embedding_dims = 50
-    epochs = 10
+    embedding_dims = 200
+    epochs = 5
 
     print('Loading data...')
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
@@ -20,25 +20,29 @@ def main():
     print(len(x_test), 'test sequences')
 
     print('Pad sequences (samples x time)...')
-    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    x_train = sequence.pad_sequences(x_train, maxlen=max_len)
+    x_test = sequence.pad_sequences(x_test, maxlen=max_len)
     print('x_train shape:', x_train.shape)
     print('x_test shape:', x_test.shape)
 
     print('Build model...')
-    model = RCNNVariant(maxlen, max_features, embedding_dims)
-    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+    model = RCNNVariant(max_len=max_len, max_features=max_features, embedding_dims=embedding_dims).get_model()
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
 
     print('Train...')
     early_stopping = EarlyStopping(monitor='val_accuracy', patience=3, mode='max')
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
-              callbacks=[early_stopping],
+              #callbacks=[early_stopping],
               validation_data=(x_test, y_test))
+    print(model.summary())
 
     print('Test...')
     result = model.predict(x_test)
+    print('result: ', result)
 
 
 
